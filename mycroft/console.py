7410f8fa00efd28caa5cc994b9ecd063bd21c666
W.P. McNeill <billmcn@gmail.com>
2017-07-07 14:50:35 -0500

    Improve help text

--- mycroft/console.py
+++ mycroft/console.py
@@ -1,4 +1,5 @@
 import argparse
+import textwrap
 
 from mycroft import __version__, train, predict
 
@@ -11,12 +12,15 @@ def main():
     subparsers = parser.add_subparsers(title="Commands")
 
     # Train subcommand
-    train_parser = subparsers.add_parser("train", description="Train a model")
+    train_parser = subparsers.add_parser("train", description=textwrap.dedent("""
+        Train a model to predict categorical labels from English text. The training data is a comma- or tab-delimited 
+        file with column of text and a column of labels. The training and optional validation loss and accuracy are
+        printed for each epoch."""))
 
-    data_group = train_parser.add_argument_group("data")
+    data_group = train_parser.add_argument_group("data", description="Arguments for specifying the data to train on")
     data_group.add_argument("training", help="training data")
     data_group.add_argument("--limit", metavar="N", type=int,
-                            help="only train on this many sample (default use all the data)")
+                            help="only train on this many samples (default use all the data)")
     data_group.add_argument("--validation", metavar="PORTION", type=float,
                             help="portion of data to use for validation (default none)")
     data_group.add_argument("--text-name", metavar="NAME", default="text",
@@ -24,21 +28,26 @@ def main():
     data_group.add_argument("--label-name", metavar="NAME", default="label",
                             help="name of the label column (default 'label')")
 
-    model_group = train_parser.add_argument_group("model")
+    model_group = train_parser.add_argument_group("model",
+                                                  description="Arguments for specifying the model configuration")
     model_group.add_argument("--rnn-units", metavar="N", type=int, default=128, help="RNN units (default 128)")
     model_group.add_argument("--dropout", metavar="RATE", type=float, default=0.2, help="Dropout rate (default 0.2)")
 
-    train_group = train_parser.add_argument_group("model")
+    train_group = train_parser.add_argument_group("training",
+                                                  description="Arguments for controlling the training procedure")
     train_group.add_argument("--epochs", metavar="N", type=int, default=10, help="training epochs (default 10)")
-    train_group.add_argument("--batch-size", metavar="N", type=int, default=56, help="batch size (default 256)")
-    train_group.add_argument("--model-filename", metavar="FILENAME", help="file in which to to store the model")
+    train_group.add_argument("--batch-size", metavar="M", type=int, default=56, help="batch size (default 256)")
+    train_group.add_argument("--model-filename", metavar="FILENAME",
+                             help="file in which to to store the model (default do not store a model)")
 
     train_parser.set_defaults(
         func=lambda args: train(args.training, args.text_name, args.label_name, args.limit, args.batch_size,
                                 args.epochs, args.rnn_units, args.dropout, args.validation, args.model_filename))
 
     # Predict subcommand
-    predict_parser = subparsers.add_parser("predict", description="Use a model to predict labels")
+    predict_parser = subparsers.add_parser("predict", description=textwrap.dedent("""
+        Use a model to predict labels. The test data is a comma- or tab-delimited file with column of text. This
+        prints that file, adding columns containing predicted probabilities for each category."""))
     predict_parser.add_argument("test", help="test data")
     predict_parser.add_argument("model_filename", metavar="filename", help="file containing the trained model")
     predict_parser.add_argument("--text-name", metavar="NAME", default="text",
