0ad7b3d4139729a48bd4234755327418f066a53f
W.P. McNeill <billmcn@gmail.com>
2017-07-27 18:36:47 -0500

    Improve setup.py
    Getting it ready to put on PyPI
    
    This contributes to #35.

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
+from subprocess import check_call
+
 from setuptools import setup
 from setuptools.command.develop import develop
 from setuptools.command.install import install
-from subprocess import check_call
 
 import mycroft
 
@@ -22,18 +23,34 @@ class PostInstallCommand(install):
         install.run(self)
 
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
 setup(
     name="mycroft",
     version=mycroft.__version__,
     packages=["mycroft"],
     url="https://github.com/wpm/mycroft",
-    license="",
+    license="MIT",
+    keywords="lstm keras spacy machine-learning natural-language-processing rnn word-embeddings",
+    python_requires=">=3.2",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+    ],
     entry_points={
         "console_scripts": ["mycroft=mycroft.console:default_main"],
     },
     author="W.P. McNeill",
     author_email="billmcn@gmail.com",
-    description="Text classifier", install_requires=["cytoolz", "keras", "numpy", "pandas", "scikit-learn", "spacy"],
+    description="Text classifier",
+    long_description=readme(),
+    install_requires=["cytoolz", "keras", "numpy", "pandas", "scikit-learn", "spacy"],
     cmdclass={
         "develop": PostDevelopCommand,
         "install": PostInstallCommand
